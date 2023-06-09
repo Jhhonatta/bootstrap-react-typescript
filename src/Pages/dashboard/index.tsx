761bde9fb6297e0ec4deeef4137cec3e2d812a61
Jhonatta Martins da Silva <jhonatta_martins@hotmail.com>
2023-06-09 12:29:23 -0300

    button navbar add

--- src/Pages/dashboard/index.tsx
+++ src/Pages/dashboard/index.tsx
@@ -1,17 +1,28 @@
 import { ContainerDashboard } from "./style";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Navbar, Nav } from "react-bootstrap";
 
 const Dashboard = () => {
   return (
-    <ContainerDashboard className="containerDashboard d-flex align-items-center">
-      <Form>
+    <ContainerDashboard className="containerDashboard">
+      <Navbar bg="light" expand="sm" className="p-3 m-0">
+        <Navbar.Brand className="mr-0 col-6">Notepad</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="w-100 d-flex justify-content-between">
+            <Button>Perfil</Button>
+            <Button>Adicionar tarefa</Button>
+            <Button>Sair</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      {/* <Form>
         <Form.Control
           placeholder="Digite aqui..."
           as="textarea"
           aria-label="With textarea"
         />
         <Button>Adicionar</Button>
-      </Form>
+      </Form> */}
     </ContainerDashboard>
   );
 };
