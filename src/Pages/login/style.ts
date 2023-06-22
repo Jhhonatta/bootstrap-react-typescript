9dcba897a65eb097dd2908d758e308f88f4269bf
Jhonatta Martins da Silva <jhonatta_martins@hotmail.com>
2023-06-22 20:49:52 -0300

    page inicial finalizada

--- src/Pages/login/style.ts
+++ src/Pages/login/style.ts
@@ -4,26 +4,36 @@ export const ContainerLogin = styled.div`
   background-color: black;
   font-family: Nunito;
 
+  .navbar-inicial {
+    background-color: rgba(255, 255, 255, 1);
+  }
+
   .title-iniciar {
     font-weight: 600;
 
     color: rgba(255, 87, 127, 1);
-    background-color: rgba(255, 255, 255, 1);
   }
 
   .container-iniciar {
-    padding: 1rem;
+    // margin-top: 45px;
     border-radius: 10px;
+    font-weight: 600;
+
     color: rgba(255, 255, 255, 1);
 
     .text-finish {
       background-color: rgba(33, 37, 41, 1);
-      width: auto;
     }
+  }
+
+  .icon-iniciar {
+    font-size: 128px;
+    color: white;
   }
 
   .button-iniciar {
     background-color: rgba(255, 87, 127, 1);
     border: 1px solid rgba(255, 87, 127, 1);
+    color: black;
   }
 `;
