82e8be01c5392927168708275c80c51a98df0c4c
Jhonatta Martins da Silva <jhonatta_martins@hotmail.com>
2023-07-06 17:41:08 -0300

    Estlização readaptada

--- src/Components/containerListTask/style.ts
+++ src/Components/containerListTask/style.ts
@@ -4,19 +4,24 @@ export const ContainerListTask = styled.div`
   .list-task {
     overflow-y: scroll;
     height: 85%;
+    border: none;
   }
 
   .list-task::-webkit-scrollbar {
-    width: 10px;
-    margin-right: 2px;
+    width: 5px;
   }
 
   .list-task::-webkit-scrollbar-thumb {
-    background-color: #888;
+    background-color: rgba(0, 0, 0, 0.25);
     border-radius: 4px;
   }
 
   .list-task::-webkit-scrollbar-thumb:hover {
     background-color: #555;
+  }
+
+  .list-group {
+    background-color: transparent;
+    border: none;
   }
 `;
